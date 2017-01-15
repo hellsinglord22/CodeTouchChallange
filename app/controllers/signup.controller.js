20d3da0da54e6e83d148e7b8c115a085fc9a657b
Hesham Adel <heshamscorpe55@gmail.com>
2017-01-15 00:11:02 +0000

    clearn up signup controller

--- app/controllers/signup.controller.js
+++ app/controllers/signup.controller.js
@@ -2,16 +2,10 @@ const User = require('../models/user');
 
 module.exports = {
 	signUserUp: signUserUp,
-	showSignUpForm: showSignUpForm
 }
 
 
 function signUserUp(request, response) {
 	response.send(request.body); 
 }
-
-function showSignUpForm(request, response) {
-	response.send('showSignUpForm');
-}
-
 
