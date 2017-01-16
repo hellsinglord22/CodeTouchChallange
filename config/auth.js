c63587958b9127627214d94fbcbed35532a4ef75
Hesham Adel <heshamscorpe55@gmail.com>
2017-01-16 21:18:07 +0000

    Add google auth configuration

---
+++ config/auth.js
@@ -0,0 +1,7 @@
+module.exports = {
+	'google' : {
+		'clientID': '677130843009-min74bpag4u0kr3b5p9d0lnal6g72qni.apps.googleusercontent.com',
+		'clientSecret': 'azXBCJ4KkWQqLiJKiAd7d83X',
+		'callbackURL': 'http://localhost:3000/auth/google/callback'
+	}
+}
